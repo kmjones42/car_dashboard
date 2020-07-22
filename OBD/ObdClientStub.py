cff49598baa71f00d4977700283eb84e5c0cf981
Kimberly <mathjones42@gmail.com>
2020-07-21 22:04:11 -0700

    Changed proto names

--- OBD/ObdClientStub.py
+++ OBD/ObdClientStub.py
@@ -6,7 +6,8 @@ import obdservice_pb2_grpc
 with grpc.insecure_channel('[::]:50052') as channel:
     stub = obdservice_pb2_grpc.ObdStub(channel)
 
-    request_data = obdservice_pb2.ObdDataTypes(speed=True, rpm=True)
+    request_data = obdservice_pb2.ObdRequestTypes(speed=True, rpm=True)
 
     for current_data in stub.StreamObd(request_data):
         print(current_data.currentspeed, " ", current_data.currentrpm)
+        
