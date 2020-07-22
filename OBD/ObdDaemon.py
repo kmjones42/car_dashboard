abba01e4bb4a963f0b45344af786bef1697a1272
Kimberly <mathjones42@gmail.com>
2020-07-21 22:05:05 -0700

    Refactored Obd server

--- OBD/ObdDaemon.py
+++ OBD/ObdDaemon.py
@@ -8,50 +8,54 @@ import grpc
 import obdservice_pb2
 import obdservice_pb2_grpc
 
-keys = ['RPM', 'SPEED']
-cmds = {key: getattr(obd.commands,key) for key in keys}
 
-
-class Obd(obdservice_pb2_grpc.ObdServicer):
-    def __init__(self):
+class Obd():
+    def __init__(self, keys):
         print("Setting up listener")
         self.connection = self.connect_obd()
-        print("Connected")
-        self.keys = []
-
-    def StreamObd(self, request, context):
-        print("Got a connection")
-        if request.speed:
-            self.keys.append("SPEED")
-        if request.rpm:
-            self.keys.append("RPM") 
+        if not self.connection.is_connected():
+            raise ConnectionError()
+        print("OBD Connected")
         self.cmds = {key: getattr(obd.commands,key) for key in keys}
 
-        return self.query_obd_data()
-
     def connect_obd(self):
-        connection = obd.OBD()
-
-        if not connection.is_connected():
-            print("Could not connect to OBD, exiting.")
-            exit(1);
-
-        return connection
+        return obd.OBD()
 
     def query_obd_data(self):
+        print("querying")
         while True:
             for key, cmd in self.cmds.items():
                 if key == 'SPEED':
-                    speed = str(connection.query(cmd).value)
+                    speed = str(self.connection.query(cmd).value)
                 if key == 'RPM':
-                    rpm = str(connection.query(cmd).value)
+                    rpm = str(self.connection.query(cmd).value)
 
-        yield route_guide_pb2.ObdData(currentspeed=speed, currentrpm=rpm)
+            yield route_guide_pb2.ObdResponse(currentspeed=speed, currentrpm=rpm)
+
+
+class ObdServer(obdservice_pb2_grpc.ObdServicer):
+
+
+    def StreamObd(self, request, context):
+        print("Got a connection")
+
+        keys = []
+        if request.speed:
+            keys.append("SPEED")
+        if request.rpm:
+            keys.append("RPM")
+
+        try:
+            obd_connection = Obd(keys)
+        except ConnectionError:
+            context.abort(grpc.StatusCode.UNAVAILABLE, "Could not connect to OBD.")
+
+        return obd_connection.query_obd_data()
 
 
 def serve_obd_data():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    obdservice_pb2_grpc.add_ObdServicer_to_server(Obd(), server)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    obdservice_pb2_grpc.add_ObdServicer_to_server(ObdServer(), server)
 
     server.add_insecure_port('[::]:50052')
     server.start()
